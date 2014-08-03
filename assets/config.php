4a7a766c254a58d634e25b131f510f50d6488b0d
Andrea Battellocchi <yellowelise@gmail.com>
2014-08-03 10:25:52 +0200

    edit for github clone

--- assets/config.php
+++ assets/config.php
@@ -6,15 +6,15 @@
  */
 $_SESSION['path'] =  (__DIR__) .DIRECTORY_SEPARATOR. "homes" .DIRECTORY_SEPARATOR;
 $_SESSION['app_path'] =  (__DIR__).DIRECTORY_SEPARATOR;
-$_SESSION['kc_path'] = "/c2s/assets/homes/";
+$_SESSION['kc_path'] = "/crypt2share/assets/homes/";
 $_SESSION['kc_denied_ext'] = "exe|com|msi|bat|php|phps|phtml|php3|php4|cgi|pl|py|js|css|prop";
 $_SESSION['not_logged'] = "location: ../index.php";
 $_SESSION['logged'] = "location: assets/kc/browse.php?lang=it";
-$_SESSION['server_path'] = "http://localhost/c2s/assets/";
+$_SESSION['server_path'] = "http://localhost/crypt2share/assets/";
 $_SESSION['temp_path'] = (__DIR__).DIRECTORY_SEPARATOR."tmp".DIRECTORY_SEPARATOR;
-$_SESSION['site_address'] = "http://localhost/c2s/";
-$_SESSION['app_address'] = "http://localhost/c2s/assets/";
-$_SESSION['mysite_address'] = "http://localhost/c2s/assets/mysite/";
-$_SESSION['public_address'] = "http://localhost/c2s/upload/";
+$_SESSION['site_address'] = "http://localhost/crypt2share/";
+$_SESSION['app_address'] = "http://localhost/crypt2share/assets/";
+$_SESSION['mysite_address'] = "http://localhost/crypt2share/assets/mysite/";
+$_SESSION['public_address'] = "http://localhost/crypt2share/upload/";
 
 ?>
